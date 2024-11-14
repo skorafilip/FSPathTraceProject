ce98eae8fea277ae0e25735bf30fcfd01772c317
skorafilip <45392453+skorafilip@users.noreply.github.com>
2024-11-14 14:21:29 +0100

    Preparing framework

---
+++ Source/FSPathTraceProject/FSPathTraceProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FSPathTraceProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FSPathTraceProject, "FSPathTraceProject" );
